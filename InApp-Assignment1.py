e5e5a9016be2822378bace9b4d72ba7ca1f95839
Ardra Rajan <90329643+Ardra-R@users.noreply.github.com>
2021-09-11 11:55:44 +0530

    Modified the code

--- InApp-Assignment1.py
+++ InApp-Assignment1.py
@@ -1,37 +1,42 @@
 import random
-choices=['rock','paper','scissors']
+choices={'rock':1,'paper':2,'scissors':3}
 print(choices)
 round=0
 p=0
 c=0
 Choices={}
 while(round<10):
-    player_choice=input('enter player choice from given list:')
-    computer_choice=random.choice(choices)
+    player_choice=int(input('enter number corresponding to players choice from given list:'))
+    computer_choice=random.choice(list(choices.values()))
+    print("Computers choice is:",computer_choice)
     if(player_choice==computer_choice):
         result='tie'
-    elif(player_choice=='rock' and computer_choice=='scissors' ):
+    elif (player_choice-computer_choice) == (-2) or (player_choice-computer_choice) == (1):
         result='player'
-    elif(player_choice=='scissors' and computer_choice=='paper'):
-        result='player'
-    elif(player_choice=='paper' and computer_choice=='rock'):
-        result='player'
+        p+=1
     else:
         result='computer'
+        c+=1
     Choices[round]=[player_choice,computer_choice,result]
-    round=round+1
-    if(result=='tie'):
-        p=p
-        c=c
-    elif(result=='player'):
-        p=p+1
-    elif(result=='computer'):
-        c=c+1
-if(result=='tie'):
+    round+=1
+if(p==c):
     print('Its a tie')
-else:
-    print('The winner is:',result)
+elif(p>c):
+    print('The winner is player')
     print('Total points earned by player:',p)
     print('Total points earned by computer:',c)
-print(Choices)
+else:
+    print('The winner is computer')
+    print('Total points earned by player:',p)
+    print('Total points earned by computer:',c)
 
+while True :
+    r=int(input('enter round  from 0 to 9 for which you need the information:'))
+    if 0<=r<10:
+        print('players choice:',Choices[r][0])
+        print('computers choice:',Choices[r][1])
+        if (Choices[r][2]=='tie'): print('Its a tie')
+        else: print('Winner is:', Choices[r][2])
+    else:
+        print('This round does not exist')
+        continue
